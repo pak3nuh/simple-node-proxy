d88c14982d333c995c82401cdf56c968bcbdaba8
nunocaro <nuno.pik@gmail.com>
2015-08-09 19:09:44 +0100

    Added host header name strategy.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ expt.Server = require('./lib/proxy-server');
 expt.Proxy = require('./lib/proxy-entry');
 expt.Strategy = require('./lib/strategies/proxy-strategy');
 expt.Strategy.Cookie = require('./lib/strategies/cookie-strategy')
+expt.Strategy.HostName = require('./lib/strategies/hostname-strategy')
 	
 
 module.exports = expt;
